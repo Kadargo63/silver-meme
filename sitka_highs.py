8195d152839380475255e45bd905d97ea032eca4
Kadargo63 <robertisaacs87@yahoo.com>
2022-09-07 21:29:12 -0500

    CSV parsing

--- sitka_highs.py
+++ sitka_highs.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-filename = 'data/navarre_weather_2017-2022.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -22,10 +22,10 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
 # Format plot.
-ax.set_title("Daily high temperatures - 2018", fontsize=24)
-ax.set_xlabel('', fontsize=16)
+plt.title("Daily high temperatures - 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-ax.set_ylabel("Temperature (F)", fontsize=16)
-ax.tick_params(axis='both', which='major', labelsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
